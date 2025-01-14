16f7f72bd49d48c5e03a5b504245ab3123baa436
trongson <trongson18101999@gmail.com>
2025-01-14 21:16:37 +0700

    fix: update width random button move after hover

--- myJs.js
+++ myJs.js
@@ -1,133 +1,129 @@
-$(document).ready(function() {
-    // process bar
-    setTimeout(function() {
-        firstQuestion();
-        $('.spinner').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    }, 600);
-})
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+});
 
-function firstQuestion(){
-    
-    $('.content').hide();
-    Swal.fire({
-        title: 'Hé luu Bấy bề!',
-        text: 'Tớ có điều này muốn hỏi cậu nhé? Phải trả lời thật lòng nhaaa!',
-        imageUrl: 'img/cuteCat.jpg',
-        imageWidth: 300,
-        imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
-        imageAlt: 'Custom image',
-      }).then(function(){
-        $('.content').show(200);
-      })
+function firstQuestion() {
+  $(".content").hide();
+  Swal.fire({
+    title: "Hé luu Bấy bề!",
+    text: "Tớ có điều này muốn hỏi cậu nhé? Phải trả lời thật lòng nhaaa!",
+    imageUrl: "img/cuteCat.jpg",
+    imageWidth: 300,
+    imageHeight: 300,
+    background: '#fff url("img/iput-bg.jpg")',
+    imageAlt: "Custom image",
+  }).then(function () {
+    $(".content").show(200);
+  });
 }
 
- // switch button position
- function switchButton() {
-    var audio = new Audio('img/sound.mp3');
-    audio.play();
-    var leftNo = $('#no').css("left");
-    var topNO = $('#no').css("top");
-    var leftY = $('#yes').css("left");
-    var topY = $('#yes').css("top");
-    $('#no').css("left", leftY);
-    $('#no').css("top", topY);
-    $('#yes').css("left", leftNo);
-    $('#yes').css("top", topNO);
+// switch button position
+function switchButton() {
+  var audio = new Audio("img/sound.mp3");
+  audio.play();
+  var leftNo = $("#no").css("left");
+  var topNO = $("#no").css("top");
+  var leftY = $("#yes").css("left");
+  var topY = $("#yes").css("top");
+  $("#no").css("left", leftY);
+  $("#no").css("top", topY);
+  $("#yes").css("left", leftNo);
+  $("#yes").css("top", topNO);
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('img/sound.mp3');
-    audio.play();
-    if (screen.width<=600) {
-        var x = Math.random() * 400;
-        var y = Math.random() * 500;
-    } else{
-        var x = Math.random() * 500;
-        var y = Math.random() * 500;
-    }
-    var left = x + 'px';
-    var top = y + 'px';
-    $('#no').css("left", left);
-    $('#no').css("top", top);
+  var audio = new Audio("img/sound.mp3");
+  audio.play();
+  if (screen.width <= 600) {
+    var x = Math.random() * 400;
+    var y = Math.random() * 500;
+  } else {
+    var x = Math.random() * 1024;
+    var y = Math.random() * 500;
+  }
+  var left = x + "px";
+  var top = y + "px";
+  $("#no").css("left", left);
+  $("#no").css("top", top);
 }
 
-
 var n = 0;
-$('#no').mousemove(function() {
-    if (n < 1)
-        switchButton();
-    if (n > 1)
-        moveButton();
-    n++;
+$("#no").mousemove(function () {
+  if (n < 1) switchButton();
+  if (n > 1) moveButton();
+  n++;
 });
-$('#no').click(() => {
-    if (screen.width>=900)
-        switchButton();
-})
+$("#no").click(() => {
+  if (screen.width >= 900) switchButton();
+});
 
 // generate text in input
 function textGenerate() {
-    var n = "";
-    var text = " Tại vì cậu đẹp trai :<< hí hí ";
-    var a = Array.from(text);
-    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
-    var count = textVal.length;
-    if (count > 0) {
-        for (let i = 1; i <= count; i++) {
-            n = n + a[i];
-            if (i == text.length + 1) {
-                $('#txtReason').val("");
-                n = "";
-                break;
-            }
-        }
+  var n = "";
+  var text = " Tại vì cậu đẹp trai :<< hí hí ";
+  var a = Array.from(text);
+  var textVal = $("#txtReason").val() ? $("#txtReason").val() : "";
+  var count = textVal.length;
+  if (count > 0) {
+    for (let i = 1; i <= count; i++) {
+      n = n + a[i];
+      if (i == text.length + 1) {
+        $("#txtReason").val("");
+        n = "";
+        break;
+      }
     }
-    $('#txtReason').val(n);
-    setTimeout("textGenerate()", 1);
+  }
+  $("#txtReason").val(n);
+  setTimeout("textGenerate()", 1);
 }
 
 // show popup
-$('#yes').click(function() {
-    var audio = new Audio('sound/tick.mp3');
-    audio.play();
-    Swal.fire({
-        title: 'Nói cho tớ lí do cậu đồng ý đi 🥺🤭',
-        html: true,
-        width: 900,
-        padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
-        backdrop: `
+$("#yes").click(function () {
+  var audio = new Audio("sound/tick.mp3");
+  audio.play();
+  Swal.fire({
+    title: "Nói cho tớ lí do cậu đồng ý đi 🥺🤭",
+    html: true,
+    width: 900,
+    padding: "3em",
+    html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
+    background: '#fff url("img/iput-bg.jpg")',
+    backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
               left top
               no-repeat
             `,
-        showCancelButton: true,
-        cancelButtonText: "Thôi ngại lém 🤭",
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonColor: '#fe8a71',
-        cancelButtonColor: '#f6cd61',
-        confirmButtonText: 'Gửi cho tớ đi ❤️'
-    }).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                width: 900,
-                confirmButtonText: 'Okiiiii lun <3',
-                background: '#fff url("img/iput-bg.jpg")',
-                title: 'Tớ biết mà ^^ Yêu cậu nhất trên đời 😘🥰😍',
-                text: "Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo hihi 😋",
-                confirmButtonColor: '#83d0c9',
-                onClose: () => {
-                    window.location = 'https://www.facebook.com/profile.php?id=100032736788526';
-                  }
-            })
-        }
-    })
-})
+    showCancelButton: true,
+    cancelButtonText: "Thôi ngại lém 🤭",
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonColor: "#fe8a71",
+    cancelButtonColor: "#f6cd61",
+    confirmButtonText: "Gửi cho tớ đi ❤️",
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire({
+        width: 900,
+        confirmButtonText: "Okiiiii lun <3",
+        background: '#fff url("img/iput-bg.jpg")',
+        title: "Tớ biết mà ^^ Yêu cậu nhất trên đời 😘🥰😍",
+        text: "Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo hihi 😋",
+        confirmButtonColor: "#83d0c9",
+        onClose: () => {
+          window.location =
+            "https://www.facebook.com/profile.php?id=100032736788526";
+        },
+      });
+    }
+  });
+});
